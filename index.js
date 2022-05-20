ef6f5248ca61e5caf4a6d2c184001d773414fb14
Thomas Cook <tlncook85@gmail.com>
2022-05-19 18:06:54 -0600

    commit tree dfb1dcaeb190ba0ebfcd754fb1157aec527a0b04

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() { 
+  const tutorialsArr = tutorials.map((tutorial) => 
+    tutorial.split(' ').map((word) =>
+      word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+  );
+  return tutorialsArr;
 }
